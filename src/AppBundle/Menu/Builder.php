3063f2b45bcc3d08307e9416d5ac136bfd4aa5b7
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2016-11-20 00:18:10 -0500

    Adjust the menu to the new template.

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -15,16 +15,19 @@ class Builder implements ContainerAwareInterface
     {
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
-        
-        // $menu->addChild('menu.homepage', [
-        //     'route' => 'homepage'
-        // ]);
+
+        $menu->addChild('menu.homepage', [
+            'route' => 'homepage'
+        ]);
 
         $this->addDestinationMenu($menu);
         
         $menu->addChild('menu.contact', [
             'route' => 'contact'
         ]);
+
+        $this->addUserMenu($menu);
+        $this->addLanguageMenu($menu, $options);
         
         return $menu;
     }
@@ -43,7 +46,7 @@ class Builder implements ContainerAwareInterface
                 $menu->addChild($location->getName(), [
                     'route' => "hostelsByDestination",
                     'routeParameters' => ['slug' => $location->getSlug()]
-                ]);
+                ])->setAttribute('dropdown_item', true);
             }
         }
     }
@@ -52,7 +55,14 @@ class Builder implements ContainerAwareInterface
     {
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
+
+        $this->addUserMenu($menu);
     
+        return $menu;
+    }
+    
+    public function addUserMenu(ItemInterface $root)
+    {
         $securityChecker = $this->container->get('security.authorization_checker');
         $translator = $this->container->get('translator');
         if ($securityChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
@@ -64,49 +74,59 @@ class Builder implements ContainerAwareInterface
             ]);
         
             // create another menu item
-            $menu->addChild('User', [
+            $menu = $root->addChild('User', [
                 'label' => $userMessage
             ])->setAttribute('dropdown', true);
         
-            $menu['User']->addChild('menu.user.edit', [
+            $menu->addChild('menu.user.edit', [
                 'route' => 'fos_user_profile_edit'
-            ])->setAttribute('icon', 'icon icon-edit');
+            ])
+                ->setAttribute('icon', 'icon icon-edit')
+                ->setAttribute('dropdown_item', true);
             
-            $menu['User']->addChild('menu.user.changePassword', [
+            $menu->addChild('menu.user.changePassword', [
                 'route' => 'fos_user_change_password'
-            ])->setAttribute('icon', 'icon icon-key');
+            ])
+                ->setAttribute('icon', 'icon icon-key')
+                ->setAttribute('dropdown_item', true);
 
             if($user->isOwner())
             {
-                $menu['User']->addChild('menu.hostel.register', [
+                $menu->addChild('menu.hostel.register', [
                     'route' => 'hostel_registration'
-                ]);
+                ])->setAttribute('dropdown_item', true);
             }
         
-            $menu['User']->addChild($translator->trans('layout.logout', [], 'FOSUserBundle'), [
+            $menu->addChild($translator->trans('layout.logout', [], 'FOSUserBundle'), [
                 'route' => 'fos_user_security_logout'
-            ])->setAttribute('icon', 'icon icon-sign-out');
+            ])
+                ->setAttribute('icon', 'icon icon-sign-out')
+                ->setAttribute('dropdown_item', true);
 
         } else {
-            $menu->addChild($translator->trans('layout.login', [], 'FOSUserBundle'), [
+            $root->addChild($translator->trans('layout.login', [], 'FOSUserBundle'), [
                 'route' => 'fos_user_security_login'
             ])->setAttribute('icon', 'icon icon-sign-in');
         }
-    
-        return $menu;
     }
-    
     
     public function languageMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
     
+        $this->addLanguageMenu($menu, $options);
+    
+        return $menu;
+    }
+
+    private function addLanguageMenu(ItemInterface $root, array $options)
+    {
         $request = $this->container->get('request_stack')->getCurrentRequest();
         $requestLocale = $request->getLocale();
         $locales = explode('|', $this->container->getParameter('app.locales'));
-        
-        $menu->addChild('Language', [
+
+        $menu = $root->addChild('Language', [
             'label' => $this->localeName($requestLocale, $requestLocale)
         ])->setAttribute('dropdown', true);
         
@@ -117,13 +137,13 @@ class Builder implements ContainerAwareInterface
                return $elem != $requestLocale; 
             });
             foreach ($locales as $locale) {
-                $menu['Language']->addChild($this->localeName($locale, $locale), [
+                $menu->addChild($this->localeName($locale, $locale), [
                     'route' => $route,
                     'routeParameters' => array_merge($routeParameters, ['_locale' => $locale])
-                ]);
+                ])->setAttribute('dropdown_item', true);
             }
         }
-    
+
         return $menu;
     }
     
