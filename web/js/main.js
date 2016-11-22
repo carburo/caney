e6ccc5672a82182ddff6c2cbb76282ee308e0fc7
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2016-11-22 02:17:21 -0500

    Make the hostel view page fit the new template

--- web/js/main.js
+++ web/js/main.js
@@ -108,6 +108,53 @@ $(document).ready(function() {
 	    return false;
 	  });
 	  
+	  $('.swipe-gallery').each( function() {
+        // Get the items.
+        var $pic = $(this),
+            getItems = function() {
+                var items = [];
+                $pic.find('a').each(function() {
+                    var $href = $(this).attr('href'),
+                        $size   = $(this).data('size').split('x'),
+                        $width  = $size[0],
+                        $height = $size[1];
+
+                    var item = {
+                        src : $href,
+                        w   : $width,
+                        h   : $height
+                    }
+
+                    items.push(item);
+                });
+                return items;
+            }
+
+        var items = getItems();
+
+        // Preload image.
+        var image = [];
+        $.each(items, function(index, value) {
+            image[index]     = new Image();
+            image[index].src = value['src'];
+        });
+
+        // Binding click event.
+        var $pswp = $('.pswp')[0];
+        $pic.on('click', 'figure', function(event) {
+            event.preventDefault();
+
+            var $index = $(this).data('order');
+            var options = {
+                index: $index,
+                bgOpacity: 1,
+                showHideOpacity: true
+            }
+
+            var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+            lightBox.init();
+        });
+    });
  
 });
 
