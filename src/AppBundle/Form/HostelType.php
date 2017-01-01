513054c47d465ba0fe6200db6ba0563e20e521c1
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2017-01-01 02:18:37 -0500

    Add admin functionalities for admin

--- src/AppBundle/Form/HostelType.php
+++ src/AppBundle/Form/HostelType.php
@@ -7,34 +7,38 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class HostelType extends AbstractType
 {
 
-//    private $tokenStorage;
-//
-//    public function __construct(TokenStorageInterface $tokenStorage)
-//    {
-//        $this->tokenStorage = $tokenStorage;
-//    }
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-//        // grab the user, do a quick sanity check that one exists
-//        $user = $this->tokenStorage->getToken()->getUser();
-//        if ($user && $user->getUsername() == 'admin') {
-//            $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($user) {
-//                $form = $event->getForm();
-//                $form
-//                    ->add('active')
-//                    ->add('owner')
-//                    ;
-//            });
-//        }
+        // grab the user, do a quick sanity check that one exists
+        $user = $this->tokenStorage->getToken()->getUser();
+        if ($user && in_array('ROLE_ADMIN', $user->getRoles())) {
+            $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($user) {
+                $form = $event->getForm();
+                $form
+                    ->add('active')
+                    ->add('owner')
+                ;
+            });
+        }
 
         $builder
             ->add('hostelName')
